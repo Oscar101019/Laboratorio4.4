ee4b8fdca4f3c095695dced46c9b3d6fcf7ed71a
Oscar <oscar101019@gmail.com>
2018-05-13 17:31:46 -0600

    Listo para revision

---
+++ app/src/main/java/com/example/android/java/MainActivity.java
@@ -0,0 +1,86 @@
+package com.example.android.java;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ImageView imageView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        imageView = (ImageView) findViewById(R.id.faceIcon);
+    }
+
+    public void onButtonClick(View v) {
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.grow);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        imageView.startAnimation(animation);
+    }
+
+    public void startAlpha(View v){
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.alpha);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        imageView.startAnimation(animation);
+    }
+
+    public void startRotate(View v){
+        Animation animation = AnimationUtils.loadAnimation(this, R.anim.rotate);
+
+        animation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+            }
+        });
+
+        imageView.startAnimation(animation);
+    }
+
+}
